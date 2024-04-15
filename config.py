2a119d4adb213ab60f989f52b6a82d0c8872f6ea
manybotts <105394368+manybotts@users.noreply.github.com>
2024-04-15 04:00:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6416830962:AAEFk0Noaz9DiK-zGPqyBEvJ36yixRyhL4g")
+API_ID = int(os.environ.get("API_ID", "5166878"))
+API_HASH = os.environ.get("API_HASH", "fdafb41f9a67f40e34a6c67f47730a92")
+OWNER = int(os.environ.get("OWNER", "762308466"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "iboxRenameBot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "iBOXTV")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001405282390"))
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://mwangy:mwangy@cluster0.n90jhk8.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
 
-STRING = os.environ.get("STRING", "")
-BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
+STRING = os.environ.get("STRING", "BAF9bj4APnVabAz0_x_o_VowMsVi5_91ciPh0MuUh28yzlQ1CGNLUUv80Zgq8chbp-Vb3dUHjeozM2N7rsNyuEtjhW_Ec05UWcdW4cYOeSGPntbMpOI0EnY-zpAzYqjjN9-X9Ur74ooKscy9IH6xkinob34muBjSKxTnyowXmvEmqAnRR3NziTmH8kw7luYgBdpb78xa02uuwJu2xyDpNhYVaal7WzZt-RqxGJleuZRrMBN6YkDWXIWxFIwy1MNgww-tIa51ccJXhEAOm10nS6AWM8gM83WgWikSNsYEwRyNxtmUK-v0wshkA1TEKMF340WJaLjC2eap3NbEoOCpv2Sp7mSgAAAAFlR5-3AA")
+BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/269146d5d4084bf50a41f.jpg")
 
 
 
