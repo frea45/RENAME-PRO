527d0fd6f0974d6a2ab6364b6698476037c17a08
frea45 <110092585+frea45@users.noreply.github.com>
2025-04-09 22:09:56 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,23 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6416830962:AAEFk0Noaz9DiK-zGPqyBEvJ36yixRyhL4g")
-API_ID = int(os.environ.get("API_ID", "5166878"))
-API_HASH = os.environ.get("API_HASH", "fdafb41f9a67f40e34a6c67f47730a92")
-OWNER = int(os.environ.get("OWNER", "762308466"))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "iboxRenameBot")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "1996499923:AAGVCjT4msY1AOHb-qtalwMsrwu0dD65k7g")
+API_ID = int(os.environ.get("API_ID", "3335796"))
+API_HASH = os.environ.get("API_HASH", "138b992a0e672e8346d8439c3f42ea78")
+#OWNER = int(os.environ.get("OWNER", "763990585"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "ir_renamebot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "iBOXTV")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001405282390"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "ir_botz")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001792962793"))
 
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://mwangy:mwangy@cluster0.n90jhk8.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://abirhasan2005:abirhasan@cluster0.i6qzp.mongodb.net/cluster0?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
 
-STRING = os.environ.get("STRING", "BAF9bj4APnVabAz0_x_o_VowMsVi5_91ciPh0MuUh28yzlQ1CGNLUUv80Zgq8chbp-Vb3dUHjeozM2N7rsNyuEtjhW_Ec05UWcdW4cYOeSGPntbMpOI0EnY-zpAzYqjjN9-X9Ur74ooKscy9IH6xkinob34muBjSKxTnyowXmvEmqAnRR3NziTmH8kw7luYgBdpb78xa02uuwJu2xyDpNhYVaal7WzZt-RqxGJleuZRrMBN6YkDWXIWxFIwy1MNgww-tIa51ccJXhEAOm10nS6AWM8gM83WgWikSNsYEwRyNxtmUK-v0wshkA1TEKMF340WJaLjC2eap3NbEoOCpv2Sp7mSgAAAAFlR5-3AA")
-BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/269146d5d4084bf50a41f.jpg")
+STRING = os.environ.get("STRING", "")
+BOT_PIC = os.environ.get("BOT_PIC", "https://telegra.ph/file/19eeb26fa2ce58765917a.jpg")
 
 
+OWNER = [int(os.environ.get("OWNER", "763990585"))]
 
 
 
